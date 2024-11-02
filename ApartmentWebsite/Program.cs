2d50e0c30fcaaa442051d66d6fbca8d307092a13
Lê Quang Khải <khailqhe170154@fpt.edu.vn>
2024-11-02 07:42:32 +0700

    build: configuration for email sending, set session and set cookie remain login

--- ApartmentWebsite/Program.cs
+++ ApartmentWebsite/Program.cs
@@ -1,8 +1,33 @@
+﻿using ApartmentWebsite.Models;
+using ApartmentWebsite.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
+var cookieExpireTime = builder.Configuration.GetValue<int>("Authentication:CookieExpireTime");
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<PRN221_PRJContext>();
+builder.Services.AddScoped<UserService>();
+builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailSettings"));
+builder.Services.AddTransient<IEmailService, EmailService>();
+// Cấu hình session
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); 
+    options.Cookie.HttpOnly = true; // Chỉ truy cập từ phía server
+    options.Cookie.IsEssential = true; // Đảm bảo cookie này được lưu 
+});
 
+// Cấu hình xác thực cookie
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/User/Sign_in";
+        options.LogoutPath = "/User/Logout";
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(cookieExpireTime); 
+        options.SlidingExpiration = true;
+    });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,7 +42,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseSession();
 app.UseAuthorization();
 
 app.MapRazorPages();
