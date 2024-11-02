182b9aa61126a69a1d37ec0e0c7031823feac9de
Lê Quang Khải <khailqhe170154@fpt.edu.vn>
2024-11-02 07:47:19 +0700

    class helper to make expiry session time flexible

---
+++ ApartmentWebsite/Helper/SessionHelper.cs
@@ -0,0 +1,47 @@
+﻿namespace ApartmentWebsite.Helper
+{
+    using System;
+    using Microsoft.AspNetCore.Http;
+    using Newtonsoft.Json;
+
+    public static class SessionHelper
+    {
+        public static void SetObjectAsJson(this ISession session, string key, object value, TimeSpan expiry)
+        {
+            // Tạo đối tượng chứa dữ liệu và thời gian hết
+            var sessionData = new SessionData
+            {
+                Value = value,
+                ExpiryTime = DateTime.UtcNow.Add(expiry)
+            };
+
+            // Chuyển đổi thành JSON để lưu vào session
+            session.SetString(key, JsonConvert.SerializeObject(sessionData));
+        }
+
+        public static T GetObjectFromJson<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            if (value == null) return default;
+
+            var sessionData = JsonConvert.DeserializeObject<SessionData>(value);
+
+            // Kiểm tra xem dữ liệu hết hạn
+            if (sessionData.ExpiryTime < DateTime.UtcNow)
+            {
+                session.Remove(key); // Xóa 
+                return default;
+            }
+
+            return (T)sessionData.Value;
+        }
+
+        // Lớp lưu giá trị và thời gian hết
+        private class SessionData
+        {
+            public object Value { get; set; }
+            public DateTime ExpiryTime { get; set; }
+        }
+    }
+} 
+
